c6af8e4449d406c572ae727d13cc82e0ab94e582
JAVA <JAVA@Krachunovi-PC>
2015-11-28 23:22:31 +0200

    Remove empty line

--- OMGTest/src/com/omg/whether/WeatherConditions.java
+++ OMGTest/src/com/omg/whether/WeatherConditions.java
@@ -65,7 +65,6 @@ public class WeatherConditions {
 	public void printInfoByCity(String city) {
 		City currentCity = getCitysByName().get(city);
 		currentCity.printAllInfoAboutCity();
-
 	}
 
 	public City cityWithMinTemperature() {
